0dfa06cbc6479c87127723647414d75a97fb4044
jjdiurno <juanjomalang@gmail.com>
2024-03-20 09:54:26 +0100

    main

--- ExamPPJyM/src/Main.java
+++ ExamPPJyM/src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("HHHHHHHHH!");
+        System.out.println("""
+                1. Dar alta vehiculo
+                2. Dar alta cliente
+                """);
 
         
     }
