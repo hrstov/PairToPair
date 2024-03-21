44cd36c64c68331fbd46e2fd7748d0e99676a1ec
[practica] <[mromcas0312@g.educaand.es]>
2024-03-21 18:53:59 +0100

    Creacion del metodo cobro en la clase Empresa

--- ExamPPJyM/src/Empresa.java
+++ ExamPPJyM/src/Empresa.java
@@ -9,51 +9,64 @@ public class Empresa {
     private List<Vehiculo> listaVehiculos;
 
 
-        public Empresa(String nombreempresa) {
-            this.nombreempresa = nombreempresa;
-            this.mapaClientes = new HashMap<>();
-            this.listaVehiculos = new ArrayList<>();
+    public Empresa(String nombreempresa) {
+        this.nombreempresa = nombreempresa;
+        this.mapaClientes = new HashMap<>();
+        this.listaVehiculos = new ArrayList<>();
+    }
+
+
+    public boolean hayCliente(String id) {
+        if (mapaClientes.containsKey(id)) {
+            return true;
         }
+        return false;
+    }
 
+    public boolean addCliente(String id, Cliente cliente) {
+        if (cliente != null && !hayCliente(id)) {
+            mapaClientes.put(id, cliente);
+            return true;
+        }
+        return false;
+    }
 
-        public boolean hayCliente(String id) {
-            if (mapaClientes.containsKey(id)) {
+    public boolean hayVehiculo(String matricula) {
+        for (Vehiculo v : listaVehiculos) {
+            if (matricula.equalsIgnoreCase(v.getMatricula())) {
                 return true;
             }
-            return false;
         }
+        return false;
+    }
 
-        public boolean addCliente(String id, Cliente cliente) {
-            if (cliente != null && !hayCliente(id)) {
-                mapaClientes.put(id, cliente);
-                return true;
-            }
-            return false;
+    public boolean addVehiculo(Vehiculo vehiculo) {
+        if (!hayVehiculo(vehiculo.getMatricula())) {
+            listaVehiculos.add(vehiculo);
+            return true;
         }
+        return false;
+    }
 
-        public boolean hayVehiculo(String matricula) {
-            for (Vehiculo v : listaVehiculos) {
-                if (matricula.equalsIgnoreCase(v.getMatricula())) {
-                    return true;
+    public float cobro(Vehiculo vehiculo, String matricula, float km) {
+        for (Vehiculo v : listaVehiculos) {
+            if (matricula.equalsIgnoreCase(v.getMatricula())) {
+                v.setKm(km);
+                if (v.getKm() > 500) {
+                    float precioMas = v.getPrecio() + (v.getPrecio() * 20 / 100);
+                    return precioMas;
+                } else {
+                    return v.getPrecio();
                 }
             }
-            return false;
-        }
+        }return 0;
+    }
 
-        public boolean addVehiculo(Vehiculo vehiculo) {
-            if (!hayVehiculo(vehiculo.getMatricula())) {
-                listaVehiculos.add(vehiculo);
-                return true;
-            }
-            return false;
-        }
-
-
-        public String getNombreempresa() {
+        public String getNombreempresa () {
             return nombreempresa;
         }
 
-        public void setNombreempresa(String nombreempresa) {
+        public void setNombreempresa (String nombreempresa){
             this.nombreempresa = nombreempresa;
         }
     }
